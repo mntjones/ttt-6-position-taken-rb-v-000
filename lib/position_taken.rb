ba2e90216e5762a5c8a0ecd6ac71770de92a1877
Monica Tamaru Jones <mntamaru@gmail.com>
2019-11-12 20:41:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,16 @@
 def position_taken? (board, index)
   
+<<<<<<< HEAD
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   
   elsif board[index] == "X" || board[index] == "O"
+=======
+  if board[index] == " " || "" || nil
+    return false
+  
+  elsif board[index] == "X" || "O"
+>>>>>>> b42a75e03a605ad648a220ebb723710f164ddafb
     return true
   end
   
