cc803338db16cbbc26729947175757d741332795
Monica Tamaru Jones <mntamaru@gmail.com>
2019-11-12 20:42:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,11 +4,6 @@ def position_taken? (board, index)
     return false
   
   elsif board[index] == "X" || board[index] == "O"
-
-  if board[index] == " " || "" || nil
-    return false
-  
-  elsif board[index] == "X" || "O"
     return true
   end
   
